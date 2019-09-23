4d941c29077264787529110b49031fd283f913a8
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-09-22 18:09:36 -0700

    almost done with tests

--- __test__/document-collection.test.js
+++ __test__/document-collection.test.js
@@ -13,15 +13,41 @@ const documentCollection = new DocumentCollection('folder');
 
 describe('Document Collection', () => {
   it('saves file', () => {
-    const newFile = {
+    const newFolder = {
       key: 'file'
     };
-    writeFile.mockResolvedValue(newFile);
+    writeFile.mockResolvedValue(newFolder);
 
-    documentCollection.save(newFile)
+    documentCollection.save(newFolder)
       .then(() => {
         expect(writeFile.mock.calls.length).toBe(1);
-        expect(writeFile.mock.calls[0][1]).toBe(JSON.stringify(newFile));
+        expect(writeFile.mock.calls[0][1]).toBe(JSON.stringify(newFolder));
+      });
+  });
+
+  it('gets file', () => {
+    const id = 'test';
+    const newFolder = {
+      key: 'file'
+    };
+    readFile.mockResolvedValue(JSON.stringify(newFolder));
+
+    return documentCollection.get(id)
+      .then(res => {
+        expect(readFile.mock.calls.length).toBe(1);
+        expect(readFile.mock.calls[0][0]).toBe('./folder/' + id + '.json');
+        expect(res).toEqual(newFolder);
+      });
+  });
+
+  it('gets all files', () => {
+    readdir.mockResolvedValue([]);
+
+    return documentCollection.getAll()
+      .then(res => {
+        expect(readdir.mock.calls.length).toBe(1);
+        expect(readdir.mock.calls[0][0]).toEqual('folder');
+        expect(res).toEqual([]);
       });
   });
 });
