e1aee242e79eed49a08858244df91fc2f0f86770
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-09-22 17:50:46 -0700

    first test

--- __test__/document-collection.test.js
+++ __test__/document-collection.test.js
@@ -1,3 +1,6 @@
+const DocumentCollection = require('../lib/document-collection');
+const path = require('path');
+
 jest.mock('../lib/files.js', () => ({
   readFile: jest.fn(),
   writeFile: jest.fn(),
@@ -6,7 +9,19 @@ jest.mock('../lib/files.js', () => ({
 
 // for setting up mock expectations
 const { readFile, writeFile, readdir } = require('../lib/files');
+const documentCollection = new DocumentCollection('folder');
 
 describe('Document Collection', () => {
-  // TODO
+  it('saves file', () => {
+    const newFile = {
+      key: 'file'
+    };
+    writeFile.mockResolvedValue(newFile);
+
+    documentCollection.save(newFile)
+      .then(() => {
+        expect(writeFile.mock.calls.length).toBe(1);
+        expect(writeFile.mock.calls[0][1]).toBe(JSON.stringify(newFile));
+      });
+  });
 });
